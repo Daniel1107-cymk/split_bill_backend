1eef0f9284ef725bfc3c414666d5af7a9906bab3
Daniel Lim <daniellim4400@gmail.com>
2021-03-21 13:08:24 +0700

    add bill and bill_details migration

---
+++ database/migrations/2021_03_21_060332_create_bills_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 100);
+            $table->date('date');
+            $table->decimal('grand_total', 15, 2);
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
---
+++ database/migrations/2021_03_21_060343_create_bill_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bill_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_name', 255);
+            $table->integer('quantity');
+            $table->decimal('price', 15, 2);
+            $table->decimal('sub_total', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bill_details');
+    }
+}
