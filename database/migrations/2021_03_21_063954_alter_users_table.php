a2a0af19a27c4bce493e4088c8f2484909544486
Daniel Lim <daniellim4400@gmail.com>
2021-03-21 13:45:43 +0700

    add alter table user migration

---
+++ database/migrations/2021_03_21_063954_alter_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('name', 'full_name');
+            $table->string('gender', 20)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('full_name', 'name');
+            $table->dropColumn('gender');
+        });
+    }
+}
