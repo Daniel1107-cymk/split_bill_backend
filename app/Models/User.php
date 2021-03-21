7f1058ca172a96272e5087b9ad0f0d655f7fd27e
Daniel Lim <daniellim4400@gmail.com>
2021-03-21 14:55:34 +0700

    add relationship function on user

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,8 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims() {
         return [];
     }
+
+    public function bill() {
+        return $this->hasMany(Bill::class);
+    }
 }
